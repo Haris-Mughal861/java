84c5a24cb79abbd740ebdc6659f95aa828333d3c
Muhammad Haris <harismughal861@gmail.com>
2024-05-09 14:25:27 +0500

    Initial commit

---
+++ Marks Calculator.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Main{
+ public static void main(String args[]){
+
+Scanner sc =new Scanner(System.in);
+System.out.println("Entre The Number 0 or 1");
+int a = sc.nextInt();
+
+
+
+if(a==1){
+    System.out.println("Entre The Marks");
+    int b = sc.nextInt();
+  
+  
+  
+    if( b>=90 ){
+System.out.println("Exellent");
+   
+
+
+}
+    if(b>=70== b<90){
+        System.out.println("Good");
+   
+   
+   
+    }
+    if( b>=50==b<70 ){
+        System.out.println("Average");
+   
+   
+   
+    }
+    if(b<50){
+        System.out.println("fail");
+    }
+  
+
+}
+else{
+
+}
+
+ }}
+
+
+
+  
